b0ff09492d48177052767374f3f8c8eb71a48575
Andrew Holder <77300461+acholder@users.noreply.github.com>
2022-01-14 17:07:55 -0800

    Add files via upload
    Fixed issue with color reverting back to default after switching to "viewing" then back to "editing" or "suggesting" mode.

--- content.js
+++ content.js
@@ -1,23 +1,55 @@
-var cursor = document.getElementById("kix-current-user-cursor-caret");
 
+window.onload = getMode;
+
+function getMode() {
+  
+var docsMode = document.getElementById('docs-toolbar-mode-switcher');
+var mode = docsMode.dataset;
 var observer = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutation) {
-        if (cursor.style.borderColor !== "rgb(76,139,245)")  {
-          cursor.style.borderColor = "rgb(76,139,245)";
-        }
-        else {
-          console.log("Border color not changed");
-        }
-    });
+    cursorColor;
+    
+    if (mode.tooltip === "Viewing mode") {
+        console.log(mode.tooltip + ", cursor disabled");
+    }
+    
+    else if (mode.tooltip !== undefined && mode.tooltip !== "Viewing mode") {
+      cursorColor();
+      console.log(mode.tooltip + ", cursor enabled.");
+    }
+    
+    else {
+      console.log("waiting on mode update...");
+    }
+
+  });
+  
 });
- 
+observer.observe(docsMode, {
+  attributes:true,
+  attributeFilter: ['data-tooltip']
+});
+
+}
+
+function cursorColor(){
+
+var gBlue = "rgb(76,139,245)";
+var cursor = document.getElementById("kix-current-user-cursor-caret");
+var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+            cursor.style.borderColor = gBlue;
+            
+          });
+});
+
 observer.observe(cursor, {
-    attributes: true
+  attributes:true
 });
+  
+//cursor.style.borderColor = gBlue;
 
-cursor.style.borderColor = "rgb(76,139,245)";
-
-
+}
 
 
 
