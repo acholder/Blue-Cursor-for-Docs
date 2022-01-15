ef5b485a8dfa450aa191c48257ec0fcbcc22bbd9
Andrew Holder <77300461+acholder@users.noreply.github.com>
2022-01-15 15:40:44 -0800

    Update content.js

--- content.js
+++ content.js
@@ -11,19 +11,16 @@ var observer = new MutationObserver(function(mutations) {
       if (mode.tooltip === "Viewing mode") {
         console.log(mode.tooltip + ", cursor disabled.");
       }
-    
       else if (mode.tooltip !== undefined && mode.tooltip !== "Viewing mode") {
         cursorColor();
         console.log(mode.tooltip + ", cursor enabled.");
       }
-    
       else {
         console.log("waiting on mode update...");
       }
-
     });
-  
   });
+  
 observer.observe(docsMode, {
   attributes:true,
   attributeFilter: ['data-tooltip']
@@ -31,24 +28,20 @@ observer.observe(docsMode, {
 
 }
 
-
-
 function cursorColor(){
 
 var cursor = document.getElementById("kix-current-user-cursor-caret");
 var gBlue = "rgb(66, 133, 244)";
 var observer = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutation) {
-    if (cursor.style.borderColor !== gBlue) {
       
-      console.log("color changed to " + cursor.style.borderColor + ", updating to " + gBlue + ".");
-      cursor.style.borderColor = gBlue;
-      console.log("color updated.");
-    }
-    
+      if (cursor.style.borderColor !== gBlue) {
+        console.log("color reverted to " + cursor.style.borderColor + ", updating to " + gBlue + ".");
+        cursor.style.borderColor = gBlue;
+        console.log("color updated.");
+      }
     });
-    
-});
+   });
 
 observer.observe(cursor, {
   attributes:true,
@@ -56,7 +49,6 @@ observer.observe(cursor, {
 });
 
 cursor.style.borderColor = gBlue;
-
 }
 
 
