565ab90b77fb886abc854df08b233361adf75cae
Andrew Holder <77300461+acholder@users.noreply.github.com>
2022-01-16 15:35:15 -0800

    Add files via upload

--- content.js
+++ content.js
@@ -1,55 +1,74 @@
 
+
 window.onload = getMode;
 
-function getMode() {
-  
-var docsMode = document.getElementById('docs-toolbar-mode-switcher');
-const mode = docsMode.dataset;
-var observer = new MutationObserver(function(mutations) {
-    mutations.forEach(function(mutation) {
-    
-      if (mode.tooltip === "Viewing mode") {
-        console.log(mode.tooltip + ", cursor disabled.");
-      }
-      else if (mode.tooltip !== undefined && mode.tooltip !== "Viewing mode") {
-        cursorColor();
-        console.log(mode.tooltip + ", cursor enabled.");
-      }
-      else {
-        console.log("waiting on mode update...");
-      }
-    });
-  });
-  
-observer.observe(docsMode, {
-  attributes:true,
-  attributeFilter: ['data-tooltip']
-});
-
-}
-
 function cursorColor(){
-
+  
 var cursor = document.getElementById("kix-current-user-cursor-caret");
 var gBlue = "rgb(66, 133, 244)";
 var observer = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutation) {
+    
+    if (cursor.style.borderColor !== gBlue) {
       
-      if (cursor.style.borderColor !== gBlue) {
-        console.log("color reverted to " + cursor.style.borderColor + ", updating to " + gBlue + ".");
-        cursor.style.borderColor = gBlue;
-        console.log("color updated.");
-      }
+      console.log("color reverted to " + cursor.style.borderColor + ".");
+      cursor.style.borderColor = gBlue;
+      console.log("Updated to " + gBlue + ".");
+    }
+    
     });
-   });
+    
+});
 
 observer.observe(cursor, {
+  
   attributes:true,
   attributeFilter: ['style']
+
 });
 
 cursor.style.borderColor = gBlue;
+
 }
 
 
+function getMode() {
+  
+cursorColor();
+  
+var docsMode = document.getElementById('docs-toolbar-mode-switcher');
+var mode = docsMode.dataset;
+var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+    
+      if (mode.tooltip === "Viewing mode") {
+        
+        console.log(mode.tooltip + ", cursor disabled.");
+        
+      }
+    
+      else if (mode.tooltip !== undefined && mode.tooltip !== "Viewing mode") {
+        
+        cursorColor();
+        console.log(mode.tooltip + ", cursor enabled.");
+       
+      }
+    
+      else {
+        
+        console.log("waiting on mode update...");
+        
+      }
 
+    });
+  
+  });
+  
+observer.observe(docsMode, {
+  
+  attributes:true,
+  attributeFilter: ['data-tooltip']
+
+});
+
+}
